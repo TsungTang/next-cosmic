1f4dd24561f601cd2772e1a3214d4ce2347e0e85
TsungTang <s710262101@gm.ntpu.edu.tw>
2021-08-15 22:33:45 +0800

    init

--- components/container.js
+++ components/container.js
@@ -1,3 +1,3 @@
 export default function Container({ children }) {
-  return <div className="container mx-auto px-5">{children}</div>
+  return <div className="container mx-auto px-2 md:px-0  md:w-[80%] lg:w-[70%] ">{children}</div>
 }
